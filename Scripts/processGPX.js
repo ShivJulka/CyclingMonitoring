404fcbaecec38ec2c98dc7c45d509deea6fa8acb
Desktop <shivjulka@gmail.com>
2023-05-11 04:44:36 +0100

    updates to locally tested GPX processing data

--- Scripts/processGPX.js
+++ Scripts/processGPX.js
@@ -1,89 +1,156 @@
+//script to loop through all gpx files in a directory and output a html file with similar activities to a seperat HTML document
 
 
 const fs = require('fs');
-const gpxParse = require('gpx-parse');
-
-const directoryPath = 'C:\\Users\\Master Yoda\\Downloads\\Commute\\';
-
-fs.readdir(directoryPath, function (err, files) {
-  if (err) {
-    return console.log('Unable to scan directory: ' + err);
-  }
-
-  let longestDistance = 0;
-  let longestDistanceFile = '';
-
-  files.forEach(function (file, index) {
-    fs.readFile(directoryPath + file, function (err, data) {
-      if (err) {
-        return console.log('Unable to read file: ' + err);
-      }
-
-      let totalTime = 0;
-
-      gpxParse.parseGpx(data, function (error, data) {
-        if (error) {
-          return console.log('Error parsing GPX data: ' + error);
-        }
-
-        const distance = data.tracks[0].segments.reduce(
-          (totalDistance, segment) => {
-            const segmentDistance = segment.reduce((distance, point, index) => {
-              if (index === 0) {
-                return distance;
-              }
-              const previousPoint = segment[index - 1];
-              return distance + getDistanceFromLatLonInKm(
-                point.lat,
-                point.lon,
-                previousPoint.lat,
-                previousPoint.lon
-              );
-            }, 0);
-            return totalDistance + segmentDistance;
-          },
-          0
-        );
-
-        const segment = data.tracks[0].segments[0];
-          const timeTaken = data.tracks[0].segments[0].timeInterval
-    ? data.tracks[0].segments[0].timeInterval.endTime - data.tracks[0].segments[0].timeInterval.startTime
-    : 0;
+const path = require('path');
+const togeojson = require('togeojson');
+const DOMParser = require('xmldom').DOMParser;
 
 
-        totalTime += timeTaken;
-
-        if (distance > longestDistance) {
-          longestDistance = distance;
-          longestDistanceFile = file;
-        }
-
-        console.log(`Distance for ${file} is ${distance.toFixed(2)} km, time is ${totalTime.toFixed(2)}`);
-      });
-    });
-  });
-
-  console.log(`The file with the longest distance is ${longestDistanceFile}.`);
-});
-
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1); // deg2rad below
-  const dLon = deg2rad(lon2 - lon1);
+//use the Haversine formula to calculate the distance between two points
+function haversineDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Earth radius in km
+  const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
       Math.sin(dLon / 2) *
       Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const d = R * c; // Distance in km
-  return d;
+  const distance = R * c;
+  return distance;
 }
 
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
+//function to apply the haversine formula to points in the GPX file
+function computeDistance(geojson) {
+  let distance = 0;
+  const coordinates = geojson.features[0].geometry.coordinates;
+
+  for (let i = 0; i < coordinates.length - 1; i++) {
+    const [lon1, lat1] = coordinates[i];
+    const [lon2, lat2] = coordinates[i + 1];
+
+    distance += haversineDistance(lat1, lon1, lat2, lon2);
+  }
+
+  return distance;
 }
 
-//cd C:\xampp\htdocs\Cycling\Scripts
-//node processGPX.js
+//function to get the duration of the activity
+function getDuration(geojson) {
+  const coordinates = geojson.features[0].geometry.coordinates;
+  const startTime = new Date(coordinates[0][2] * 1000);
+  const endTime = new Date(coordinates[coordinates.length - 1][2] * 1000);
+  const duration = (endTime - startTime) / 1000; // in seconds
+
+  return duration;
+}
+
+//function to group the similae journeys
+function groupBy(journeys, distanceTolerance, timeTolerance) {
+  const groups = {};
+
+  journeys.forEach((journey) => {
+    let foundGroup = false;
+
+    Object.keys(groups).forEach((groupId) => {
+      const refJourney = groups[groupId][0];
+
+      if (
+        Math.abs(journey.distance - refJourney.distance) <= distanceTolerance &&
+        Math.abs(journey.duration - refJourney.duration) <= timeTolerance
+      ) {
+        groups[groupId].push(journey);
+        foundGroup = true;
+      }
+    });
+
+    if (!foundGroup) {
+      const groupId = Object.keys(groups).length + 1;
+      groups[groupId] = [journey];
+    }
+  });
+
+  return groups;
+}
+
+//write the external HTML file
+function writeHtmlOutput(groups, outputHtml) {
+  let htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Similar Activities</title>
+</head>
+<body>
+<h1>Similar Activities</h1>
+`;
+
+  Object.keys(groups).forEach((groupId) => {
+    const group = groups[groupId];
+    htmlContent += `<h2>Group ${groupId}</h2><ul>`;
+    group.forEach((journey) => {
+      htmlContent += `<li>${journey.file}: ${journey.distance.toFixed(
+        2
+      )} km, ${journey.duration.toFixed(2)} s</li>`;
+    });
+    htmlContent += '</ul>';
+  });
+
+  htmlContent += '</body></html>';
+
+  fs.writeFileSync(outputHtml, htmlContent);
+}
+
+
+//read the GPX file
+function readGpxFile(filePath) {
+  try {
+    const gpxFile = fs.readFileSync(filePath, 'utf8');
+    const gpxDom = new DOMParser().parseFromString(gpxFile);
+    const geojson = togeojson.gpx(gpxDom);
+    return geojson;
+  } catch (error) {
+    console.error(`Error parsing ${filePath}: ${error}`);
+    return null;
+  }
+}
+
+//main script
+const directoryPath = 'C://xampp//htdocs//CyclingMonitoring//activity';
+const outputHtml = 'similar_activities.html';
+
+const journeys = [];
+
+// Loop through all the files in the directory
+fs.readdirSync(directoryPath).forEach((filename) => {
+  if (filename.endsWith('.gpx')) {
+    const filePath = path.join(directoryPath, filename);
+    const geojson = readGpxFile(filePath);
+
+    if (!geojson) return;
+
+    const distance = computeDistance(geojson); // Compute the total distance in km
+    const duration = getDuration(geojson); // Compute the total duration in seconds
+
+    const journey = {
+      file: filename,
+      distance: distance,
+      duration: duration,
+    };
+
+    journeys.push(journey);
+  }
+});
+
+
+// Group the journeys by distance and duration
+
+const distanceTolerance = 0.1; // in km
+const timeTolerance = 300; // in seconds
+const groupedJourneys = groupBy(journeys, distanceTolerance, timeTolerance);
+
+writeHtmlOutput(groupedJourneys, outputHtml);
